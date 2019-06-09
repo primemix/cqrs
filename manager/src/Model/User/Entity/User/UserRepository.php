662e506a2c7af7338c9f13692ed5e19895f5aeb1
Prime Mix <monstrpro@gmail.com>
2019-06-09 22:33:50 +0300

    add confirm test

--- manager/src/Model/User/Entity/User/UserRepository.php
+++ manager/src/Model/User/Entity/User/UserRepository.php
@@ -5,14 +5,11 @@ declare(strict_types=1);
 namespace App\Model\User\Entity\User;
 
 
-class UserRepository
+interface  UserRepository
 {
-    public function hasByEmail(Email $email): bool
-    {
+    public function findByConfirmToken(string $token): ?User;
 
-    }
-    public function add(User $user): void
-    {
+    public function hasByEmail(Email $email): bool;
 
-    }
+    public function add(User $user): void;
 }
