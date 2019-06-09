fe1cd7d5633f7e7fe3db1d95c7d6f21386593d89
Prime Mix <monstrpro@gmail.com>
2019-06-09 21:33:28 +0300

    Add signUp test

--- manager/tests/Unit/Model/User/Entity/User/SignUp/RequestTest.php
+++ manager/tests/Unit/Model/User/Entity/User/SignUp/RequestTest.php
@@ -18,8 +18,12 @@ class RequestTest extends TestCase
             $id = Id::next(),
             $date = new DateTimeImmutable(),
             $email = new Email('test@app.test'),
-            $hash = 'hash'
+            $hash = 'hash',
+            $token = 'token'
         );
+
+        self::assertTrue($user->isWait());
+        self::assertFalse($user->isActive());
 
         self::assertEquals($id, $user->getId());
         self::assertEquals($date, $user->getDate());
